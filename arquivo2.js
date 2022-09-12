d472956668d2fb3892ad7b225518443534182e7c
Luiz Paulo Colombiano <lpcolombiano@gmail.com>
2022-09-12 05:02:17 -0300

    Update arquivo2.js
    qualquer bobagem

--- arquivo2.js
+++ arquivo2.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const corpo1 = {name:"cabeça", model:"superior"}
+const corpo2 = {name:"perna", model:"inferior"}
+const corpo3 = {name:"nariz", model:"sentidos"}
+
+
+console.log("Hello world, olá braziiil!");
+console.table([corpo1, corpo2, corpo3]);
+console.table([corpo1, corpo2, corpo3], ["model"]);
