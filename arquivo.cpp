edf98ffdd8344323e9c633be66a6ed068196752b
Colombiano <lpcolombiano@gmail.com>
2022-09-12 05:19:33 -0300

    arquivo em cpp

---
+++ arquivo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main ()
+
+{
+
+// sempre se faz necessário declarar a variável, dizendo primeiro
+// qual é o seu tipo (int, char, float, bool, etc) seguido
+// do nome da variável, algo como int x; onde declaramos uma variável
+// de no x do tipo inteiro. Só depois disso eu posso atribuir algum
+// valor a váriavel e/ou fazer outra operação nela
+
+
+return 0;
+}
+
